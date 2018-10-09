37fd3a9d93f29a643070f283d97c03a05cac31a7
bleed-it-out@live.com <bleed-it-out@live.com>
2018-10-09 15:29:19 -0300

    feat(src): add products route

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const index = require('./routes/index');
 const clientRoute = require('./routes/clientRoute');
+const productRoute = require('./routes/productRoute');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -15,5 +16,6 @@ app.use(bodyParser.json());
 
 app.use('/', index);
 app.use('/client', clientRoute);
+app.use('/product', productRoute);
 
 module.exports = app;
